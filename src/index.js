b0d6f6633cf27bb88750d9790a30be9badf1f784
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-01-11 15:35:12 +0530

    Add method to toggle whether task is done or not.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import createProject from './modules/createProject';
 
 
 const project1 = new createProject('soham');
-const todo1 = new createTodo('soham', 'sfef', 13, 1);
-const todo2 = new createTodo('sowafcwam', 'sfef', 131, 12);
+const todo1 = new createTodo('soham', 'sfef', 13, 1, false);
+const todo2 = new createTodo('sowafcwam', 'sfef', 131, 12, false);
 
 
 project1.addTodo(todo1);
 project1.addTodo(todo2);
 
 console.log(project1);
-// console.log(todo1);
 
-project1.delete(todo1);
+todo2.toggleTaskDone();
 console.log(project1);
+
 
--- src/modules/createTodo.js
+++ src/modules/createTodo.js
@@ -1,8 +1,13 @@
 export default class todos {
-	constructor(title, description, dueDate, priority) {
+	constructor(title, description, dueDate, priority, taskDone) {
 		this.title = title;
 		this.description = description;
 		this.dueDate = dueDate;
 		this.priority = priority;
+		this.taskDone = taskDone;
+	}
+
+	toggleTaskDone() {
+		this.taskDone = !(this.taskDone);
 	}
 };
