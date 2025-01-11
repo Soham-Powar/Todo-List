e9b896b7fe1d78caedd0dd765059c89b69754f67
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-01-11 17:55:37 +0530

    Create dialog to receive task details.

--- src/dom/menu.js
+++ src/dom/menu.js
@@ -1,9 +1,38 @@
 import './dom.css'
 
 const menu = document.querySelector('.menu');
+const defaultMenu = document.createElement('div');
+const projectsMenu = document.createElement('div');
+
+defaultMenu.classList.add('default-menu');
+projectsMenu.classList.add('projects-menu');
+
+projectsMenu.innerHTML = `
+	<div>
+		<h3>Projects</h3>
+		<button class = 'add-project-btn'>+</button>
+	</div>
+`;
+
+const inbox = document.createElement('div');
+const today = document.createElement('div');
+const thisWeek = document.createElement('div');
+
+inbox.innerHTML = 'Inbox'
+today.innerHTML = 'Today'
+thisWeek.innerHTML = 'This Week'
+
+defaultMenu.appendChild(inbox);
+defaultMenu.appendChild(today);
+defaultMenu.appendChild(thisWeek);
+
+
+// const addProjectBtn = document.querySelector('.add-project-btn');
+// addProjectBtn.addEventListener('click', () => {
+
+// });
 
 export default function renderMenu() {
-	menu.innerHTML = `
-		Todo List
-	`;
+	menu.appendChild(defaultMenu);
+	menu.appendChild(projectsMenu);
 }
