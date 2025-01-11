44417d703c90018bdf3894f805e3f9a5f56d8c02
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-01-11 15:41:19 +0530

    Install date-fns package.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import createTodo from './modules/createTodo';
 import createProject from './modules/createProject';
 
 
-const project1 = new createProject('soham');
+const inbox = new createProject('Inbox');
 const todo1 = new createTodo('soham', 'sfef', 13, 1, false);
 const todo2 = new createTodo('sowafcwam', 'sfef', 131, 12, false);
 
 
-project1.addTodo(todo1);
-project1.addTodo(todo2);
+inbox.addTodo(todo1);
+inbox.addTodo(todo2);
 
-console.log(project1);
+console.log(inbox);
 
 todo2.toggleTaskDone();
-console.log(project1);
+console.log(inbox);
 
 
