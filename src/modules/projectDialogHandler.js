09b29aff2fd9acc56becc3a52af59716d932cd8d
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-01-22 23:42:20 +0530

    Complete dialog handler module.

---
+++ src/modules/projectDialogHandler.js
@@ -0,0 +1,22 @@
+export default function handleProjectDialog(allProjectsArray, renderNewProject) {
+	const addProjectBtn = document.querySelector('.add-project-btn');
+	const addProjectDialog = document.querySelector('.add-project-dialog');
+	const addProjectForm = document.querySelector('.add-project-dialog form');
+
+	addProjectBtn.addEventListener('click', () => {
+		addProjectDialog.showModal();
+	});
+
+	addProjectForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const title = e.target.project_title.value;
+
+		const newProject = new createProject(title);
+		allProjectsArray.push(newProject);
+		addProjectForm.reset();
+		addProjectDialog.close();
+
+		console.log(allProjectsArray);
+		renderNewProject();
+	});
+}
