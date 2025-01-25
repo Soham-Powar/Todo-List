1e03af3f54ef3ee5455e407c5a8aed8e0203c8a8
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-01-25 19:18:24 +0530

    Modify example todos.

--- src/modules/initProjects.js
+++ src/modules/initProjects.js
@@ -13,9 +13,9 @@ export default function initProjects() {
 
 		allProjectsArray.push(inbox, today, thisWeek);
 
-		const todo1 = new createTodo('soham', 'sfef', '2025-01-18', 'low', false);
-		const todo2 = new createTodo('socwam', 'sfef', '2025-01-17', 'medium', false);
-		const todo3 = new createTodo('sowafcwam', 'sfef', '2025-01-24', 'high', false);
+		const todo1 = new createTodo('Fix bugs in user authentication', 'do it asap', '2025-01-18', 'low', false);
+		const todo2 = new createTodo('Write a summary of annual report', 'do it', '2025-01-17', 'medium', false);
+		const todo3 = new createTodo('Drink water', 'do it everyday', '2025-01-24', 'high', false);
 
 		inbox.addTodo(todo1);
 		inbox.addTodo(todo2);
